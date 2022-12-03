50798ff6ead3a8accc980770346c50cf1ebf46f4
RajathRPAI <117572868+RajathRPAI@users.noreply.github.com>
2022-12-03 11:02:14 +0530

    practice_programs

---
+++ altitude.py
@@ -0,0 +1,7 @@
+height= int(input(" WHAT IS YOUR CURRENT ALTITUDE\t"))
+if height>=10000:
+    print("go around and continue the journey ")
+elif height<=5000:
+    print("land the plane by bringing into 1000ft")
+elif height==1000:
+    print("land the plane now")
